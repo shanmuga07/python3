a4d31ebb34b5fac13d27bda73d70a07f3ea8d798
shanmuga07 <36722947+shanmuga07@users.noreply.github.com>
2018-02-23 10:41:13 +0530

    Create oddreven.py

---
+++ oddreven.py
@@ -0,0 +1,8 @@
+a=int(input("Please Enter a Number : "));  
+if(1<= a <= 100000):  
+    if a%2==0 :
+      print("This Number is Even")
+    else :
+      print("This Number is Odd")
+else:  
+    print("Invalid Input") 
